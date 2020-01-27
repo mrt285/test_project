ed7084f48d4a8076296ad73a7e349f4afd0216ab
mrt285 <martinosgr90@msn.com>
2020-01-27 18:01:43 +0100

    add file6

---
+++ file6.js
@@ -0,0 +1,22 @@
+var b = 99;
+var c = 199;
+var d = 1000;
+var g = 9347934;
+
+// blablablablablsdkfnalksdjflaksdjflaksjdf
+//
+//
+//
+//
+// asdf
+// asd
+// fa
+// s
+//
+//
+// as
+// df
+// a
+// sdf
+//
+//
